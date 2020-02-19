9112629cdc60c4ca3808478267f65b5bdfeb3a4a
Utkarsh Lohani <utkarsh.lohani@mavenwave.local>
2020-02-19 16:59:13 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { TranslateService } from "./translate.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  text: string;
+  translatedText: string;
+  constructor(private translateService: TranslateService) {}
+  submit() {
+    this.translateService.translate(this.text).subscribe(result => {
+      this.translatedText = result;
+    });
+  }
+}
